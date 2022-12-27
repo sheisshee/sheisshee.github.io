a5e282c6494aa3a63586f4358f3e367be90793a2
Sherrelyn <chavezsherrelyn13@gmail.com>
2022-12-26 17:39:56 -0800

    Added animation to home text and modified the files.

--- js/script.js
+++ js/script.js
@@ -12,15 +12,3 @@
         navbar.classList.toggle('active');
     }
      
-    // Dark Mode
-    let darkmode = document.querySelector('#darkmode');
-     
-    darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
-            document.body.classList.add('active');
-        }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
-            document.body.classList.remove('active');
-        }
-    }
