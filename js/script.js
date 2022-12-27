bd63d6aa0611027713fcfc28fe99314bde211f4f
Sherrelyn <chavezsherrelyn13@gmail.com>
2022-12-26 18:34:05 -0800

    Added darkmode.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,17 @@
     menu.onclick = () => {
         navbar.classList.toggle('active');
     }
+
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
      
